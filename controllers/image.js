c09778d005665d216127f5c9bc4922469ee0757b
Moses_pt <tolulopemoses@aol.com>
2023-11-21 22:18:28 +0100

    Fixed entries count

--- controllers/image.js
+++ controllers/image.js
@@ -6,7 +6,7 @@ const handleImage = (db) => (req, res) => {
         .increment("entries", 1)
         .returning("entries")
         .then((entries) => {
-            res.json(entries[0].entries);
+            res.json(entries[0]);
         })
         .catch(() => res.status(400).json("Unable to get entries"));
 };
